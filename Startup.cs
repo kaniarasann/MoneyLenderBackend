035d2e98ecc975bfb3fa8a2168e44a0a8c6dd832
Kaniarasan Nagalingam <kaniarasan.nagalingam@Aspiresys.com>
2018-08-21 18:25:34 +0530

    Added JWT

--- Startup.cs
+++ Startup.cs
@@ -1,29 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
 
-namespace MoneyLenderBackend {
-    public class Startup {
+namespace MoneyLenderBackend
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices (IServiceCollection services) {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                builder => builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+                .Build());
+            });
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(opt =>
+            {
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateAudience = false,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
+
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
-            if (env.IsDevelopment ()) {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseExceptionHandler(appBuilder =>
+            {
+                appBuilder.Use(async (context, next) =>
+               {
+                   var error = context.Features[typeof(IExceptionHandlerFeature)] as IExceptionHandlerFeature;
+
+                //when authorization has failed, should retrun a json message to client
+                if (error != null && error.Error is SecurityTokenExpiredException)
+                   {
+                       context.Response.StatusCode = 401;
+                       context.Response.ContentType = "application/json";
+
+                       await context.Response.WriteAsync(JsonConvert.SerializeObject(new
+                       {
+                           State = "Unauthorized",
+                           Msg = "token expired"
+                       }));
+                   }
+                //when orther error, retrun a error message json to client
+                else if (error != null && error.Error != null)
+                   {
+                       context.Response.StatusCode = 500;
+                       context.Response.ContentType = "application/json";
+                       await context.Response.WriteAsync(JsonConvert.SerializeObject(new
+                       {
+                           State = "Internal Server Error",
+                           Msg = error.Error.Message
+                       }));
+                   }
+                //when no error, do next.
+                else await next();
+               });
+            });
+            app.UseCors("CorsPolicy");
+            app.UseAuthentication();
             app.UseMvc();
-            // app.Run (async (context) => {
-            //     await context.Response.WriteAsync ("Hello World!");
-            // });
         }
     }
 }
